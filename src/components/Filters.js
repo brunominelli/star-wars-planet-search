87dd56ae1507259caf492df3e5a741cbe55f692c
Bruno Minelli <minellibruno@hotmail.com>
2022-04-19 17:50:28 -0300

    Requisito 05 concluído

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import AppContext from '../context/AppContext';
 import { operators, optionsNumericValues } from '../data';
 
@@ -6,57 +6,79 @@ function Filters() {
   const [column, setColumn] = useState('population');
   const [comparison, setComparison] = useState('maior que');
   const [value, setValue] = useState('0');
+  const [enabledColumns, setEnabledColumns] = useState([...optionsNumericValues]);
 
-  const { filterByName, filterName, filterNumericValues } = useContext(AppContext);
+  const {
+    filterByName,
+    filterName,
+    filterNumericValues,
+    filterByNumericValues,
+  } = useContext(AppContext);
+
+  useEffect(() => {
+    const columns = filterByNumericValues.map((option) => option.column);
+    const filters = optionsNumericValues
+      .filter((option) => !columns.includes(option));
+    setEnabledColumns(filters);
+  }, [filterByNumericValues]);
 
   const handleChange = ({ target }) => {
     filterName(target.value);
   };
 
   return (
-    <form>
-      <label htmlFor="name-filter">
+    <>
+      <form>
+        <label htmlFor="name-filter">
+          <input
+            type="text"
+            id="name-filter"
+            name="name-filter"
+            value={ filterByName.name }
+            onChange={ handleChange }
+            data-testid="name-filter"
+          />
+        </label>
+        <select
+          data-testid="column-filter"
+          value={ column }
+          onChange={ (e) => setColumn(e.target.value) }
+        >
+          {enabledColumns.map((option) => (
+            <option key={ option }>{option}</option>
+          ))}
+        </select>
+        <select
+          data-testid="comparison-filter"
+          value={ comparison }
+          onChange={ (e) => setComparison(e.target.value) }
+        >
+          {operators.map((option) => (
+            <option key={ option }>{option}</option>
+          ))}
+        </select>
         <input
-          type="text"
-          id="name-filter"
-          name="name-filter"
-          value={ filterByName.name }
-          onChange={ handleChange }
-          data-testid="name-filter"
+          type="number"
+          data-testid="value-filter"
+          value={ value }
+          onChange={ (e) => setValue(e.target.value) }
         />
-      </label>
-      <select
-        data-testid="column-filter"
-        value={ column }
-        onChange={ (e) => setColumn(e.target.value) }
-      >
-        {optionsNumericValues.map((option) => (
-          <option key={ option }>{option}</option>
+        <button
+          type="button"
+          data-testid="button-filter"
+          onClick={ () => filterNumericValues(column, comparison, value) }
+        >
+          Filter
+        </button>
+      </form>
+      <div>
+        {filterByNumericValues.map((filter, index) => (
+          <p key={ index }>
+            { `${filter.column} ${filter.comparison} ${filter.value}` }
+          </p>
         ))}
-      </select>
-      <select
-        data-testid="comparison-filter"
-        value={ comparison }
-        onChange={ (e) => setComparison(e.target.value) }
-      >
-        {operators.map((option) => (
-          <option key={ option }>{option}</option>
-        ))}
-      </select>
-      <input
-        type="number"
-        data-testid="value-filter"
-        value={ value }
-        onChange={ (e) => setValue(e.target.value) }
-      />
-      <button
-        type="button"
-        data-testid="button-filter"
-        onClick={ () => filterNumericValues(column, comparison, value) }
-      >
-        Filter
-      </button>
-    </form>
+      </div>
+    </>
   );
 }
 
