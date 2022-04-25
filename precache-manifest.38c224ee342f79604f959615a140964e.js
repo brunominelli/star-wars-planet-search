05b8924c3d096b8fdf6e79413e42009952012937
Bruno Minelli <minellibruno@hotmail.com>
2022-04-25 17:44:46 -0300

    Updates

---
+++ precache-manifest.38c224ee342f79604f959615a140964e.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a6505da0ebfa74fa5743a13cd96c39a",
+    "url": "/star-wars-planet-search/index.html"
+  },
+  {
+    "revision": "ead5a2a3ab54c69512a1",
+    "url": "/star-wars-planet-search/static/css/main.ed0f75f2.chunk.css"
+  },
+  {
+    "revision": "9a66717ec32e24ed8fb6",
+    "url": "/star-wars-planet-search/static/js/2.c40979da.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/star-wars-planet-search/static/js/2.c40979da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ead5a2a3ab54c69512a1",
+    "url": "/star-wars-planet-search/static/js/main.d469deb8.chunk.js"
+  },
+  {
+    "revision": "430eadddf8313e3d766d",
+    "url": "/star-wars-planet-search/static/js/runtime-main.2983b150.js"
+  },
+  {
+    "revision": "857c5b6e226e42786915b644809e825b",
+    "url": "/star-wars-planet-search/static/media/email-white-icon.857c5b6e.png"
+  },
+  {
+    "revision": "eee19cacd7eb74ef602b0affee630824",
+    "url": "/star-wars-planet-search/static/media/github-white-icon.eee19cac.png"
+  },
+  {
+    "revision": "3b8104fcb6cb4176878811d17bca77a7",
+    "url": "/star-wars-planet-search/static/media/linkedin-white-icon.3b8104fc.png"
+  },
+  {
+    "revision": "80c92b9c3263624456368156748baa07",
+    "url": "/star-wars-planet-search/static/media/telegram-white-icon.80c92b9c.png"
+  }
+]);
